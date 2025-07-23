2e40fd86438b9f7a4b0130664a2eb57943807515
Cristian Forchino <luigisinbigotes@users.noreply.github.com>
2025-07-23 16:29:51 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("uk-trip-cache").then(cache => {
+      return cache.addAll([
+        "./itinerario_uk_2025_noclickflash.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
